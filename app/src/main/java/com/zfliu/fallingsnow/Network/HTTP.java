7edbd5c7d607e2d2a619b6df586478ff805f525e
zfliu <zfliu_6@163.com>
2016-12-19 21:26:47 +0800

    添加http

---
+++ app/src/main/java/com/zfliu/fallingsnow/Network/HTTP.java
@@ -0,0 +1,99 @@
+package com.zfliu.fallingsnow.Network;
+
+import java.io.IOException;
+
+import okhttp3.FormBody;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+/**
+ * Created by zfliu on 12/19/2016.
+ */
+
+public class HTTP {
+    private static String TAG_PHONE = "Number";
+    private static String TAG_CONTENT = "Content";
+
+    private static String host = "192.168.191.1";
+    private static int port = 8012;
+    private static String GetAct = "getInfo";
+    private static String PostAct = "postInfo";
+
+    public static void Post(final String number, final String text, final OnHttpStatusListener listener){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                post(number,text,listener);
+            }
+        }).start();
+    }
+    private static void post(String number,String text,OnHttpStatusListener listener){
+        OkHttpClient client = new OkHttpClient();
+        RequestBody body = new FormBody.Builder()
+                .add(TAG_PHONE, number)
+                .add(TAG_CONTENT, text)
+                .build();
+        HttpUrl url = new HttpUrl.Builder()
+                .scheme("http")
+                .host(host).port(8012)
+                .addPathSegment(PostAct)
+                .build();
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+
+        if (listener == null){
+            listener = new OnHttpStatusListener();
+        }
+        try {
+            Response response = client.newCall(request).execute();
+            if (response.isSuccessful()){
+                String ret = response.body().string();
+                listener.Ok(ret);
+            }
+        } catch (IOException e){
+            listener.Error();
+            e.printStackTrace();
+        }
+    }
+    public static void Get(final String number, final OnHttpStatusListener listener){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                get(number,listener);
+            }
+        }).start();
+    }
+    private static void get(String number,OnHttpStatusListener listener){
+        OkHttpClient client = new OkHttpClient();
+        HttpUrl url = new HttpUrl.Builder()
+                .scheme("http")
+                .host(host).port(8012)
+                .addPathSegment(GetAct)
+                .addQueryParameter(TAG_PHONE,number)
+                .build();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        if (listener == null){
+            listener = new OnHttpStatusListener();
+        }
+        try {
+            Response response = client.newCall(request).execute();
+            String ret = response.body().string();
+            listener.Ok(ret);
+        } catch (IOException e) {
+            e.printStackTrace();
+            listener.Error();
+        }
+    }
+
+    public static class OnHttpStatusListener {
+        public void Ok(String text){}
+        public void Error(){}
+    }
+}
--- app/src/main/java/com/zfliu/fallingsnow/Porfermor/MainActivity.java
+++ app/src/main/java/com/zfliu/fallingsnow/Porfermor/MainActivity.java
@@ -1,13 +1,14 @@
 package com.zfliu.fallingsnow.Porfermor;
 
 import android.content.Intent;
-import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
+import com.cunoraz.gifview.library.GifView;
+import com.zfliu.fallingsnow.Network.HTTP;
 import com.zfliu.fallingsnow.R;
-import com.zfliu.fallingsnow.View.MarqueeTextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,13 +19,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         intent = new Intent(this,MainService.class);
+        GifView gif = (GifView)findViewById(R.id.gif);
+        gif.setGifResource(R.raw.chr);
+        final TextView tv = (TextView)findViewById(R.id.textView);
     }
 
     public void OnClickA(View view){
         startService(intent);
-        Intent MyIntent = new Intent(Intent.ACTION_MAIN);
-        MyIntent.addCategory(Intent.CATEGORY_HOME);
-        startActivity(MyIntent);
+//        Intent MyIntent = new Intent(Intent.ACTION_MAIN);
+//        MyIntent.addCategory(Intent.CATEGORY_HOME);
+//        startActivity(MyIntent);
     }
 
     public void OnClickB(View view){
