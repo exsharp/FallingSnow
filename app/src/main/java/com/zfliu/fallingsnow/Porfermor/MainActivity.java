f34018a2beca3068b5ad904fc5aa1ddb6a9e1b40
leojacky <leo_jacky@163.com>
2016-12-19 21:06:51 +0800

    添加引导界面

--- app/src/main/java/com/zfliu/fallingsnow/Porfermor/MainActivity.java
+++ app/src/main/java/com/zfliu/fallingsnow/Porfermor/MainActivity.java
@@ -1,10 +1,13 @@
 package com.zfliu.fallingsnow.Porfermor;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.zfliu.fallingsnow.R;
 import com.zfliu.fallingsnow.View.MarqueeTextView;
@@ -17,8 +20,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         intent = new Intent(this,MainService.class);
+
+        /**
+         * 1、判断是否是安装后的首次启动
+         */
+
+        SharedPreferences pref = getSharedPreferences("fallingSnowPref", MODE_PRIVATE);
+        if (pref.getBoolean("startFirst", true)) {
+            Intent intent = new Intent(this, GuideActivity.class);
+            startActivity(intent);
+
+            SharedPreferences.Editor editor = pref.edit();
+            editor.putBoolean("startFirst", false);
+            editor.commit();
+            finish();
+        }else{
+            Toast.makeText(this, "欢迎来到主界面", Toast.LENGTH_SHORT).show();
+        }
+
 //        startService(intent);
 //        final MarqueeTextView view = (MarqueeTextView)findViewById(R.id.marqueeTextView);
 //        new Thread(new Runnable() {
@@ -46,4 +66,10 @@ public class MainActivity extends AppCompatActivity {
     public void OnClickB(View view){
         stopService(intent);
     }
+
+    public void OnClickC(View view){
+        Intent intent = new Intent(this,GuideActivity.class);
+        startActivity(intent);
+    }
+
 }
